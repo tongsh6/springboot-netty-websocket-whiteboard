dec6b92e83f941ac030a33979fa3049d5dd2a1cd
tongshuanglong <tongsh6@qq.com>
2019-02-20 20:01:31 +0800

    创建项目

--- src/main/java/com/tongsh6/whiteboard/websocket/WebSocketServerIndexPage.java
+++ src/main/java/com/tongsh6/whiteboard/websocket/WebSocketServerIndexPage.java
@@ -2,6 +2,8 @@ package com.tongsh6.whiteboard.websocket;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.util.FileCopyUtils;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -14,20 +16,10 @@ public class WebSocketServerIndexPage {
     private static final String NEWLINE = "\r\n";
 
     public static ByteBuf getContent() throws IOException {
-        InputStream is = WebSocketServerIndexPage.class.getClass().getResourceAsStream("/static/index.html");
-        return Unpooled.copiedBuffer(readFully(is));
+        ClassPathResource cpr = new ClassPathResource("static/index.html");
+        byte[] indexHtml = FileCopyUtils.copyToByteArray(cpr.getInputStream());
+        return Unpooled.copiedBuffer(indexHtml);
     }
-
-    private static byte[] readFully(InputStream input) throws IOException {
-        byte[] buffer = new byte[8192];
-        int bytesRead;
-        ByteArrayOutputStream output = new ByteArrayOutputStream();
-        while ((bytesRead = input.read(buffer)) != -1) {
-            output.write(buffer, 0, bytesRead);
-        }
-        return output.toByteArray();
-    }
-
     private WebSocketServerIndexPage() {
         // Unused
     }
